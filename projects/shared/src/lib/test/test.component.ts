22efc4f7ddeed3aefb683d8922a34f84f6344c6c
Ross Lehr <itsme@rosslehr.com>
2025-03-22 17:14:19 -0400

    Initial Angular library setup

---
+++ projects/shared/src/lib/test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-test',
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+
+}
